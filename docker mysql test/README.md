c5dd5c8937e54494e0b1434b39297b125282e3df
Hash <Jess@Hashui-MacBook-Pro.local>
2018-01-08 21:09:23 +0900

    docker mysql test

---
+++ docker mysql test/README.md
@@ -0,0 +1,5 @@
+# This is Study Room Objective-C , swift language Study 
+- docker mysql + java 연동 TEST 
+- mysql-connector-java-5.1.45-bin.jar 라이브러리 사용
+
+
---
+++ docker mysql test/docker_DB_test.java
@@ -0,0 +1,47 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class docker_DB_test {
+
+    public static void main(String[] args)
+    {
+        try{
+            Connection con = null;
+
+            //String url = "jdbc:mysql://127.0.0.1:4444:3306";
+            //String user = "root";
+            //String password = "jess1130";
+
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:4444", "root" , "jess1130");
+
+            java.sql.Statement st = null;
+            ResultSet rs = null;
+            st = con.createStatement();
+            rs = st.executeQuery("SHOW DATABASES");
+
+            if(st.execute("SHOW DATABASES"))
+            {
+                rs = st.getResultSet();
+            }
+            while (rs.next())
+            {
+                String str = rs.getNString(1);
+                System.out.println(str);
+            }
+
+        }
+        catch (SQLException sqex)
+        {
+            System.out.println("SQLException" + sqex.getMessage());
+            System.out.println("SQLState : " + sqex.getSQLState());
+        }
+
+
+
+
+    }
+}
+
+
